6e98c54e040e5a3309fc8bc9dc95adec910049a7
Temperedscarf9 <95205568+Temperedscarf9@users.noreply.github.com>
2025-06-17 23:52:13 +0800

    Update ARGB8888.py

--- RGBAd32x8888eB/ARGB8888.py
+++ RGBAd32x8888eB/ARGB8888.py
@@ -33,3 +33,23 @@ class ARGB8888:
 
         with open(file_path, 'wb') as f:
             f.write(data)
+
+
+# 编码示例：
+# if __name__ == "__main__":
+    # 将图像保存为ARGB8888格式
+    # input_png_path = "/content/UI_SEEDPACKETS_2048_1536_2048_4096.png"
+    # image = Image.open(input_png_path).convert("RGBA")
+    # ARGB8888.write(image, 'output.ptx')
+
+# 解码示例：
+if __name__ == "__main__":
+    # 配置图像参数
+    ptx_path = "/content/UI_SEEDPACKETS_1536_00.PTX"
+    output_path = 'UI_SEEDPACKETS.png'
+    width  = 2048      # 2048
+    height = 4096      # 4096  # 你需要确认高度！
+    # 解码并保存为 PNG
+    image = ARGB8888.read(ptx_path, width, height)
+    image.save(output_path)
+    print("转换完成，PNG 保存为:", output_path)
